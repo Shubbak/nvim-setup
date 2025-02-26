c8daad43e7a7a8c5fd2c97bc31c9571cc2e1a7e4
Abdulrahman Shubbak <a.shubbak@outlook.com>
2025-02-26 14:12:06 +0100

    started lua configuration, added plugins to plugins.lua removed from init.vim

---
+++ lua_settings.lua
@@ -0,0 +1,6 @@
+require("settings")  -- Load options
+require("keymaps")   -- Load keybindings
+require("plugins")   -- Load plugins
+require("language")  -- Load language-settings
+require("status")    -- Load statusline settings
+require("folding")   -- Load folding settings
