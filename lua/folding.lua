33e25fff24ace43bb5cf8c1a3ce89d50a7d30316
Abdulrahman Shubbak <a.shubbak@outlook.com>
2025-03-06 07:47:21 +0100

    changed foldlevel

--- lua/folding.lua
+++ lua/folding.lua
@@ -2,5 +2,5 @@ vim.o.foldmethod = "expr"
 vim.opt.foldexpr = "v:lua.vim.treesitter.foldexpr()"
 vim.opt.foldtext = "v:lua.vim.treesitter.foldtext()"
 vim.o.foldenable = true  -- Start with folds open
-vim.o.foldlevel = 2      -- Expand all folds by default
-vim.o.foldlevelstart = 2 -- Keeps folds open when opening a file
+vim.o.foldlevel = 3      -- Expand all folds by default
+vim.o.foldlevelstart = 3 -- Keeps folds open when opening a file
