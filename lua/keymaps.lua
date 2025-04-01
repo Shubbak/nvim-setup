8666f8d661c4318606730f49fb1f1723b9a31571
Abdulrahman Shubbak <a.shubbak@outlook.com>
2025-04-01 13:01:30 +0200

    added keymap for pomo sessions

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -62,7 +62,8 @@ vim.keymap.set("n", "<leader>lp", ":lua vim.diagnostic.goto_prev()<CR>", {norema
 vim.keymap.set("n", "<leader>ln", ":lua vim.diagnostic.goto_next()<CR>", {noremap = true, desc = "Go to next error"})
 
 -- Pomodoroe specific keybindings
-vim.keymap.set("n", "<leader>ps", ":TimerSession<CR>", {noremap = true, desc = "Start Pomodoro session"})
+vim.keymap.set("n", "<leader>ps8", ":TimerSession eight<CR>", {noremap = true, desc = "Start Pomodoro session"})
+vim.keymap.set("n", "<leader>ps6", ":TimerSession six<CR>", {noremap = true, desc = "Start Pomodoro session"})
 vim.keymap.set("n", "<leader>ph", ":TimerHide<CR>", {noremap = true, desc = "Hide Pomodoro timer"})
 vim.keymap.set("n", "<leader>pp", ":TimerPause<CR>", {noremap = true, desc = "Pause Pomodoro timer"})
 vim.keymap.set("n", "<leader>pr", ":TimerResume<CR>", {noremap = true, desc = "Resume Pomodoro timer"})
